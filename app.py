87de37476bf42ba98b595600738b95ff60181358
Stanley Lin <slin55055@gmail.com>
2017-09-26 19:14:44 -0400

    finished

--- app.py
+++ app.py
@@ -1,63 +1,18 @@
 from flask import Flask, render_template
 import csv
 import random
+import percent
 
 app = Flask(__name__)
 
 
 
-
-
-occupations = {}
-
-percents = []
-
-total = 0.0
-
-with open("occupations.csv", "r") as file:
-	reader = csv.reader(file)
-	
-	#toss out the first row
-	reader.next()
-	
-	for x in reader:
-		occupations[x[0]] = float(x[1])
-		
-		if x[0] != "Total":
-			percents.append([float(x[1]), x[0]])
-		else:
-			total = float(x[1])
-
-
-
-#scale by 99.8
-for x in range(0, len(percents)):
-	percents[x][0] /= total
-
-def sign(x, y):
-	if x > y:
-		return 1
-	return -1
-
-#the idea is to get a cumulative count
-percents.sort(sign)
-
-for x in range(0, len(percents) - 1):
-	percents[x+1][0] += percents[x][0]
-
-def getOccupation():
-	rand = random.random()
-
-	for x in percents:
-		if rand <= x[0]:
-			return x
-
-
 @app.route('/occupations')
 def occupations():
-	u = []
-	u.append({"name":"test", "percent":6.7})
-	return render_template('index', occupations=u)
+	o = percent.getOccupations()
+	return render_template('index', 
+		occupations=o, 
+		randomOccupation=percent.getRandomOccupation(o))
 
 if __name__ == "__main__":
 	app.debug = True
---
+++ percent.py
@@ -0,0 +1,48 @@
+import csv
+import random
+
+def getOccupations():
+	occupations = {}
+
+	with open("data/occupations.csv", "r") as file:
+		reader = csv.reader(file)
+			
+		#toss out the first row
+		reader.next()
+		
+		for x in reader:
+			occupations[x[0]] = float(x[1])
+			
+		return occupations
+
+def sign(x, y):
+	if x > y:
+		return 1
+	return -1
+
+
+def getRandomOccupation(occupations):
+	percents = []
+	total = 0.0
+
+	for elem in occupations:
+		if elem == "Total":
+			total = float(occupations[elem])
+		#else:
+		percents.append([float(occupations[elem]), elem])
+
+	for x in range(0, len(percents)):
+		percents[x][0] /= total
+
+	#the idea is to get a cumulative count
+	percents.sort(sign)
+
+	for x in range(0, len(percents) - 1):
+		percents[x+1][0] += percents[x][0]
+
+	rand = random.random()
+
+	for x in percents:
+		if rand <= x[0]:
+			return x[1]
+
